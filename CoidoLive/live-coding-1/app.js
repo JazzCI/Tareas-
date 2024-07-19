3c8d6d39376c5b831114fa37360f85102d679c8b
JazzCI <145305085+JazzCI@users.noreply.github.com>
2024-07-19 11:14:40 -0600

    CodigoLive
    Intento de CodigoLive

---
+++ CoidoLive/live-coding-1/app.js
@@ -0,0 +1,11 @@
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+const bigWords = ( String, myArray ) => {
+    let aux = 0;
+    for (let i = 0; i < myArray.length; i++) {
+        if (i[0]) {
+            
+        }
+        
+    }
+};
